45031b8b6cc4e2e0e3beeb3d023094707c0bb542
Daniil-Padiryakkov <daniilpadiryakov@gmail.com>
2021-03-23 18:36:13 +0300

    design

--- index.php
+++ index.php
@@ -11,7 +11,42 @@
 </head>
 <body>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-white">
+                <div class="card-body">
+
+                    <form>
+
+                        <div class="form-group">
+                            <label>Email address</label>
+                            <input type="email" class="form-control" placeholder="Enter email">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" placeholder="Password">
+                        </div>
+
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input">
+                            <label class="form-check-label">Check me out</label>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <p class="lead">
+                            Нет аккаунта? - <a href="#">зарегистрируйтесь</a>!
+                        </p>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 </body>
 </html>
+
